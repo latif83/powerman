c9fd5c1dac5d843148bbb59191cf1697a0ae9fef
latif83 <latifm8360@gmail.com>
2024-02-25 18:49:41 +0000

    updates

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,20 +1,29 @@
+"use client";
 import {
   faFacebook,
   faWhatsapp,
   faYoutube,
 } from "@fortawesome/free-brands-svg-icons";
 import {
+  faBarsStaggered,
   faLocationDot,
   faMapLocationDot,
+  faXmark,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import Link from "next/link";
-import styles from './Header.module.css'
+import styles from "./Header.module.css";
+import { useState } from "react";
 
 export const Header = () => {
+  const [showNav, setShowNav] = useState(false);
+
   return (
-    <div style={{ zIndex: 9999999 }} className={`absolute top-0 w-full text-white ${styles.container}`}>
+    <div
+      style={{ zIndex: 9999999 }}
+      className={`absolute top-0 w-full text-white ${styles.container}`}
+    >
       <div className="sm:flex hidden justify-between sm:px-12 px-3 border-b">
         <span className="flex gap-2 text-sm items-center">
           <FontAwesomeIcon
@@ -29,38 +38,42 @@ export const Header = () => {
         <div className="flex">
           <div className="border-l py-2 px-3 hover:bg-black">
             <Link href="/">
-            <FontAwesomeIcon
-              icon={faFacebook}
-              width={20}
-              height={20}
-              className="text-xl"
-            />
+              <FontAwesomeIcon
+                icon={faFacebook}
+                width={20}
+                height={20}
+                className="text-xl"
+              />
             </Link>
           </div>
           <div className="border-l py-2 px-3 hover:bg-black">
-            <Link href="https://www.youtube.com/@prophetpowermanbekoe1295" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon
-              icon={faYoutube}
-              width={20}
-              height={20}
-              className="text-xl"
-            />
+            <Link
+              href="https://www.youtube.com/@prophetpowermanbekoe1295"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faYoutube}
+                width={20}
+                height={20}
+                className="text-xl"
+              />
             </Link>
           </div>
           <div className="border-l py-2 px-3 hover:bg-black">
             <Link href="/">
-            <FontAwesomeIcon
-              icon={faWhatsapp}
-              width={20}
-              height={20}
-              className="text-xl"
-            />
+              <FontAwesomeIcon
+                icon={faWhatsapp}
+                width={20}
+                height={20}
+                className="text-xl"
+              />
             </Link>
           </div>
         </div>
       </div>
 
-      <header className="flex justify-between sm:px-12 px-3 items-center">
+      <header className="flex relative justify-between sm:px-12 px-3 items-center">
         <Image
           src="/logo.png"
           width={200}
@@ -69,12 +82,62 @@ export const Header = () => {
           className="w-100"
         />
         <nav className="sm:flex gap-4 font-semibold hidden">
-          <Link className="text-red-500 border-b border-red-500" href="/">HOME</Link>
-          <Link className="hover:text-red-500" href="/">MINISTRIES</Link>
-          <Link className="hover:text-red-500" href="#sermons">SERMONS</Link>
-          <Link className="hover:text-red-500" href="#events">EVENTS</Link>
-          <Link className="hover:text-red-500" href="#getInTouch">CONTACT</Link>
+          <Link className="text-red-500 border-b border-red-500" href="/">
+            HOME
+          </Link>
+          <Link className="hover:text-red-500" href="/">
+            MINISTRIES
+          </Link>
+          <Link className="hover:text-red-500" href="#sermons">
+            SERMONS
+          </Link>
+          <Link className="hover:text-red-500" href="#events">
+            EVENTS
+          </Link>
+          <Link className="hover:text-red-500" href="#getInTouch">
+            CONTACT
+          </Link>
         </nav>
+        <div className="block sm:hidden">
+          <FontAwesomeIcon onClick={()=>setShowNav(true)} icon={faBarsStaggered} className="text-2xl" />
+          {showNav && (
+            <div
+              style={{
+                zIndex: 99999999,
+                height: "100svh",
+                backgroundColor: "#956203",
+              }}
+              className="absolute w-full top-0 left-0"
+            >
+              <div className="my-10 mx-3">
+                <div className="flex justify-end w-full">
+                  <FontAwesomeIcon
+                  onClick={()=>setShowNav(false)}
+                    icon={faXmark}
+                    className="text-4xl text-red-400"
+                  />
+                </div>
+                <nav className="flex flex-col">
+                  <Link className="hover:text-red-500 mt-5" href="/" onClick={()=>setShowNav(false)} >
+                    HOME
+                  </Link>
+                  <Link className="hover:text-red-500 mt-5" href="/" onClick={()=>setShowNav(false)} >
+                    MINISTRIES
+                  </Link>
+                  <Link className="hover:text-red-500 mt-5" href="#sermons" onClick={()=>setShowNav(false)} >
+                    SERMONS
+                  </Link>
+                  <Link className="hover:text-red-500 mt-5" href="#events" onClick={()=>setShowNav(false)} >
+                    EVENTS
+                  </Link>
+                  <Link className="hover:text-red-500 mt-5" href="#getInTouch" onClick={()=>setShowNav(false)} >
+                    CONTACT
+                  </Link>
+                </nav>
+              </div>
+            </div>
+          )}
+        </div>
       </header>
     </div>
   );
